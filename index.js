dbe2bbe9531e004734ff181cc4e75bee1ddc230c
Mankirat Singh <mankirat1998@gmail.com>
2017-11-25 23:37:45 +0530

    feat: add and find added from database

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
-const mongoose= require('mongoose');
-const User= require('./models/user');
+/*jshint esversion: 6 */
+const mongoose = require('mongoose');
+const User = require('./models/user');
 
-mongoose.Promise= global.Promise;
+mongoose.Promise = global.Promise;
 
 //connect to db
-const db= mongoose.connect('mongodb://localhost:8000/user-cli',{useMongoClient: true});
+const db = mongoose.connect('mongodb://localhost:8000/user-cli', {useMongoClient: true});
+
+//Add user
+const addUser = (user) => {
+    User.create(user).then(user => {
+        console.log("User Created");
+        db.close();
+    });
+};
+
+//Find User
+const findUser = (name) => {
+    const search = new RegExp(name, 'i');
+    User.find({$or: [{firstname: search}, {lastname: search}]}).then(
+        user => {
+            console.log(user);
+            console.log(`${user.length} matches`);
+            db.close();
+        }
+    );
+}
+
+module.exports={
+    findUser,
+    addUser
+};
+
+
