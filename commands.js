824a7a0fd0af088a9895cb25c47b7260a48da2e1
Mankirat Singh <mankirat1998@gmail.com>
2017-11-25 23:58:40 +0530

    feat: inquirerjs added for interface enhancement

--- commands.js
+++ commands.js
@@ -1,23 +1,50 @@
+/*jshint esversion: 6 */
 const program = require('commander');
+const {prompt} = require('inquirer');
 const {addUser, findUser} = require('./index');
+
+//User Questions
+const questions = [
+    {
+        type: 'input',
+        name: 'firstname',
+        message: 'User firstname:'
+    },
+    {
+        type: 'input',
+        name: 'lastname',
+        message: 'User lastname:'
+    },
+    {
+        type: 'input',
+        name: 'phone',
+        message: 'User phone:'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'User email:'
+    }
+];
 
 program
     .version('1.0.0')
-    .description('User Management System')
+    .description('User Management System');
 
+// With inquirer
 program
-    .command('add <firstname> <lastname> <phone> <email>')
+    .command('add')
     .alias('a')
-    .description('add a user')
-    .action((firstname, lastname, phone, email) => {
-        addUser({firstname,lastname,phone,email});
+    .description('Add a user')
+    .action(() => {
+        prompt(questions).then(answers => addUser(answers));
     });
 
 program
     .command('find <name>')
     .alias('f')
     .description('find user by name')
-    .action((name)=>{
+    .action((name) => {
         findUser(name);
     });
 
